9c7de01d08328237235930804d7f2ae565ae0e9d
praveeniiht <64822037+praveeniiht@users.noreply.github.com>
2020-05-22 18:00:03 +0530

    Add files via upload

--- projects/src/test/java/ControllerTestCases/UserControllerTest.java
+++ projects/src/test/java/ControllerTestCases/UserControllerTest.java
@@ -9,31 +9,54 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import com.examples.projects.controller.HomeController;
 import com.examples.projects.service.UserService;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserControllerTest {
 	
-	private MockMvc mockMvc;
-	
-	@Mock
-	private UserService userService;
-
 	@org.mockito.InjectMocks
-	private HomeController homeController;
-	
+	private HomeController userController;
+
+	private MockMvc mockMvc;
+
 	@Before
 	public void setup() {
+		System.out.println("Before Every method....");
 		MockitoAnnotations.initMocks(this);
-		this.mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
+		this.mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+	}
+
+	@Test
+	public void testHomePage() throws Exception {
+	this.mockMvc.perform(get("/validateUsers")).andExpect(status().isOk()).
+		andExpect(view().name("home"));
+	}
+
+	@Test
+	public void testredirectHomePage() throws Exception {
+	this.mockMvc.perform(get("/insertUser")).andExpect(status().isOk())
+		.andExpect(view().name("home"));
 	}
 	
 	@Test
-	public void testLoadingPageUrl() throws Exception {
-		//this.mockMvc.perform(get("/")).andExpect(status().isOk());
+	public void testgiftorder() throws Exception {
+	this.mockMvc.perform(get("/newOrder")).andExpect(status().isOk())
+		.andExpect(view().name("newgiftorder"));
 	}
-
-	
+	@Test
+	public void testplacetorder() throws Exception {
+	this.mockMvc.perform(get("/newOrder")).andExpect(status().isOk())
+		.andExpect(view().name("orderconfirm"));
+	}
+	@Test
+	public void testlogout() throws Exception {
+	this.mockMvc.perform(get("/logout")).andExpect(status().isOk())
+		.andExpect(view().name("home"));
+	}
+		
 }
